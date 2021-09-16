93b98bbac178f5abe20aa4a8df2e366dd6df1fe2
leeton1412 <leighton.griffith87@gmail.com>
2021-09-16 20:44:57 +0100

    Add appointment component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {RiFolder5Line} from "react-icons/ri"
 import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointments";
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl"><RiFolder5Line className="inline-block text-red-400 align-top"/>Your Appointment</h1>
+      <h1 className="text-5xl"><RiFolder5Line className="inline-block text-red-400 align-top mb-10"/>Your Appointment</h1>
+      <AddAppointment />
       <Search />
     </div>
   );
