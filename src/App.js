cb6e49452ab2712816e8900fdfddc8e0bcc80576
leeton1412 <leighton.griffith87@gmail.com>
2021-09-16 19:47:55 +0100

    Added Search Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import {RiFolder5Line} from "react-icons/ri"
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl"><RiFolder5Line className="inline-block text-red-400"/>Test</h1>
+      <h1 className="text-5xl"><RiFolder5Line className="inline-block text-red-400 align-top"/>Your Appointment</h1>
+      <Search />
     </div>
   );
 }
