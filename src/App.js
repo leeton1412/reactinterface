82fc198c43a4201278b4c6801c30d95cc03dc39d
leeton1412 <leighton.griffith87@gmail.com>
2021-09-16 18:53:12 +0100

    installed Tailwind css and react-icons, tested

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import {RiFolder5Line} from "react-icons/ri"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Your Appointments</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl"><RiFolder5Line className="inline-block text-red-400"/>Test</h1>
     </div>
   );
 }
