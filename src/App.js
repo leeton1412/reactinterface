996260641bcf95962f3a78db95136523e5e51d83
leeton1412 <leighton.griffith87@gmail.com>
2021-09-16 21:35:36 +0100

    Passed appointment variable through another comp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import {RiFolder5Line} from "react-icons/ri"
-import { BiTrash } from "react-icons/bi";
 import Search from "./components/Search";
 import AddAppointment from "./components/AddAppointments";
 import AppointmentList from "./data.json";
+import AppointmentInfo from "./components/Appointmentinfo";
 
 
 function App() {
@@ -16,19 +16,9 @@ function App() {
         {AppointmentList
         
         .map(appointment => (
-          <li className="px-3 py-3 flex items-start">
-          <button type="button"
-            className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-            <BiTrash /></button>
-          <div className="flex-grow">
-            <div className="flex items-center">
-              <span className="flex-none font-medium text-2xl text-blue-500">{appointment.petName}</span>
-              <span className="flex-grow text-right">{appointment.aptDate}</span>
-            </div>
-            <div><b className="font-bold text-blue-500">Owner:</b> {appointment.ownerName}</div>
-            <div className="leading-tight">{appointment.aptNotes}</div>
-          </div>
-        </li>
+            <AppointmentInfo key={appointment.id}
+            appointment={appointment}
+            />
         ))}
 
       </ul>
