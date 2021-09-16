cfa2db2c516d9505961a897244c155e59d15153b
leeton1412 <leighton.griffith87@gmail.com>
2021-09-16 19:55:14 +0100

    Added Drop Down Info

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,33 @@
-import { BiSearch, BiCaretDown } from "react-icons/bi" //Imported From React-icons
+import { BiSearch, BiCaretDown, BiCheck } from "react-icons/bi" //Imported From React-icons
 
+// Dropdown Links Needs To Be Called After Button In Search.
+const DropDown = () => {
+    return (
+        <div className="origin-top-right absolute right-0 mt-2 w-56
+        rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+        <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+          <div
+            className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+            role="menuitem">Pet Name <BiCheck /></div>
+          <div
+            className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+            role="menuitem">Owner Name  <BiCheck /></div>
+          <div
+            className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+            role="menuitem">Date <BiCheck /></div>
+          <div
+            className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer border-gray-1 border-t-2"
+            role="menuitem">Asc <BiCheck /></div>
+          <div
+            className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+            role="menuitem">Desc <BiCheck /></div>
+        </div>
+      </div>
+    )
+}
 
-const Search = () => { // Search Call Back Function
+// Search Component
+const Search = () => {
     return ( // Return The Following HTML //
         <div className="py-5"> 
         <div className="mt-1 relative rounded-md shadow-sm"> 
@@ -17,6 +43,7 @@ const Search = () => { // Search Call Back Function
                 className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
                 Sort By <BiCaretDown className="ml-2" />
               </button>
+              <DropDown />
             </div>
           </div>
         </div>
