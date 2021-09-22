652a73fe987636ce098f57a3f219fcd335b6369d
leeton1412 <leighton.griffith87@gmail.com>
2021-09-22 13:24:57 +0100

    Moved data.json to public and used callBack

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import {RiFolder5Line} from "react-icons/ri"
 import Search from "./components/Search";
 import AddAppointment from "./components/AddAppointments";
-import AppointmentList from "./data.json";
-import AppointmentInfo from "./components/Appointmentinfo";
 
+import AppointmentInfo from "./components/Appointmentinfo";
+import { useEffect, useState, useCallback } from "react";
 
 function App() {
+
+  let [appointmentList, setAppointmentList] =  useState ([]);
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+    .then(Response => Response.json())
+    .then(data => {
+      setAppointmentList(data)
+    })
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl"><RiFolder5Line className="inline-block text-red-400 align-top mb-10"/>Your Appointment</h1>
@@ -13,7 +28,7 @@ function App() {
       <Search />
 
       <ul className="divide-y divide-grey-400">
-        {AppointmentList
+      {appointmentList
         
         .map(appointment => (
             <AppointmentInfo key={appointment.id} // Passed Via Component
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,11 @@
 import { BiSearch, BiCaretDown, BiCheck } from "react-icons/bi" //Imported From React-icons
+import { useState } from "react"
 
 // Dropdown Links Needs To Be Called After Button In Search.
-const DropDown = () => {
+const DropDown = ({toggle}) => {
+    if (!toggle) {
+        return null;
+    }
     return (
         <div className="origin-top-right absolute right-0 mt-2 w-56
         rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
@@ -28,6 +32,7 @@ const DropDown = () => {
 
 // Search Component
 const Search = () => {
+    let [toggleSort, setToggleSort] = useState(false);
     return ( // Return The Following HTML //
         <div className="py-5"> 
         <div className="mt-1 relative rounded-md shadow-sm"> 
@@ -39,11 +44,11 @@ const Search = () => {
             className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
           <div className="absolute inset-y-0 right-0 flex items-center">
             <div>
-              <button type="button"
+              <button type="button" onClick={() => {setToggleSort(!toggleSort)}}
                 className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
                 Sort By <BiCaretDown className="ml-2" />
               </button>
-              <DropDown />
+              <DropDown toggle={toggleSort} />
             </div>
           </div>
         </div>
